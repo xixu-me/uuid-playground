16de285cfef05c846c3f939b74b8bcf6b5b6c04f
Xi Xu <i@xi-xu.me>
2026-03-17 15:40:34 +0800

    fix: align dev dependency majors on eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  {
+    ignores: ['dist', '.eslintrc.cjs'],
+  },
+  js.configs.recommended,
+  ...tsPlugin.configs['flat/recommended'],
+  reactHooks.configs.flat.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+    },
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+]
